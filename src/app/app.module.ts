f4519675c6cab990eb30a322c533265ab4be33a0
diepz <you@example.cm>
2018-12-12 08:15:09 +0700

    calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontSizeComponent } from './font-size/font-size.component';
 import { PetComponent } from './pet/pet.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FontSizeComponent,
-    PetComponent
+    PetComponent,
+    CalculatorComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
